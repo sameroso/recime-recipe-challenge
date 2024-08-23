f451ebfb12cd1d8910f13140d34bdfc5c0309efd
Samer Kayali <samerkkr@gmail.com>
2024-08-23 13:23:35 -0300

    refactor: moved trending recipe to templates

---
+++ app/templates/TrendingRecipesList.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { Recipe } from "../_dtos/Recipe";
+import { DefaultTabButtons, Tab, Tabs } from "../_components";
+import { CardContainer } from "../styles";
+import { Card } from "../_components/Card";
+
+export default function TrendingRecipes(props: { recipes: Recipe[] }) {
+  const [tabId, setTabId] = useState("123");
+
+  const { recipes = [] } = props;
+  return (
+    <div>
+      <Tabs value={tabId} onChangeTab={(tabId) => setTabId(tabId as string)}>
+        <Tab
+          tabId="123"
+          ButtonProps={{ children: "tab1" }}
+          component={DefaultTabButtons.start}
+        />
+        <Tab
+          tabId="234"
+          ButtonProps={{ children: "tab2" }}
+          component={DefaultTabButtons.middle}
+        />
+        <Tab
+          tabId="13"
+          ButtonProps={{ children: "tab5" }}
+          component={DefaultTabButtons.end}
+        />
+      </Tabs>
+      <CardContainer>
+        {recipes.map((recipe) => {
+          return (
+            <Card key={recipe.name} style={{ width: "200px" }}>
+              <div
+                style={{
+                  position: "relative",
+                  height: "100px",
+                  width: "100%",
+                }}
+              >
+                <Image alt={recipe.name} src={recipe.imageSrc} fill />
+              </div>
+              <div>{recipe.name}</div>
+              <div>{recipe.difficulty}</div>
+            </Card>
+          );
+        })}
+      </CardContainer>
+    </div>
+  );
+}
