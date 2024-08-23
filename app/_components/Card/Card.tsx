06ebbf9ce3fc5a663e6145b3cd5560d40d6d8e52
Samer Kayali <samerkkr@gmail.com>
2024-08-23 12:43:29 -0300

    feat: created card component

---
+++ app/_components/Card/Card.tsx
@@ -0,0 +1,5 @@
+import { CardContainer } from "./CardStyles";
+
+export function Card(props: JSX.IntrinsicElements["div"]) {
+  return <CardContainer {...props} />;
+}
---
+++ app/_components/Card/CardStyles.tsx
@@ -0,0 +1,7 @@
+import { styled } from "@/libs/styled-components";
+
+export const CardContainer = styled.div`
+  border-radius: 5px;
+  border: 1px solid black;
+  position: relative;
+`;
