fc8ac1d55c09bab95ac9f50eb7a87f1ad4c07e61
Samer Kayali <samerkkr@gmail.com>
2024-08-26 15:42:38 -0300

    fix: changed default image to fetch from cdn

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ddg0cip9uom1w.cloudfront.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
